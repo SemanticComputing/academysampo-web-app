05a1cf020805c4d983e813f5a9029e1763de151c
Petri Leskinen <petri.leskinen@aalto.fi>
2020-02-20 16:34:25 +0200

    Update Jena Query

--- src/server/sparql/JenaQuery.js
+++ src/server/sparql/JenaQuery.js
@@ -1,21 +1,25 @@
 import { runSelectQuery } from './SparqlApi'
-import { prefixes } from './sampo/SparqlQueriesPrefixes'
-import { endpoint, jenaQuery } from './SparqlQueriesGeneral'
+import { prefixes } from './as/SparqlQueriesPrefixes'
+import { jenaQuery } from './SparqlQueriesGeneral'
 import { makeObjectList } from './SparqlObjectMapper'
+import { endpoint } from './as/FacetConfigsAs'
 
 export const queryJenaIndex = async ({
   queryTerm,
   resultFormat
 }) => {
+  console.log(queryTerm)
   let q = jenaQuery
   q = q.replace('<QUERY>', `
   ?id text:query ('${queryTerm.toLowerCase()}' 2000) .
   `)
+  console.log(endpoint)
   const results = await runSelectQuery({
     query: prefixes + q,
-    endpoint,
+    endpoint: endpoint,
     resultMapper: makeObjectList,
     resultFormat
   })
+  console.log(results)
   return results
 };

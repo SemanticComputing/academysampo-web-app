14b4665bf88c10af285437ae475799655b00a9b9
Petri Leskinen <petri.leskinen@aalto.fi>
2020-10-07 15:59:14 +0300

    Adjust network node and label sizes

--- src/client/configs/as/Cytoscape.js/NetworkConfig.js
+++ src/client/configs/as/Cytoscape.js/NetworkConfig.js
@@ -4,7 +4,7 @@ export const cytoscapeStyle = [
     selector: 'node',
     style: {
       shape: 'ellipse',
-      'font-size': '12',
+      'font-size': ele => ele.data('font_size') || 12,
       'background-color': ele => ele.data('color') || '#666',
       label: ' data(prefLabel)',
       height: ele => (ele.data('size') || 16 / (ele.data('distance') + 1) || '16px'),
@@ -66,15 +66,83 @@ export const coseLayout = {
   minTemp: 1.0
 }
 
+class ValueScaler {
+  a;
+  b;
+  constructor (low, high) {
+    this.low = low
+    this.high = high
+  }
+
+  fit (vals) {
+    const valmin = Math.min(...vals)
+    const valmax = Math.max(...vals)
+    if (valmax === valmin) {
+      this.a = 0.0
+    } else {
+      this.a = (this.high - this.low) / (valmax - valmin)
+    }
+    this.b = this.low - valmin * this.a
+  }
+
+  transform (vals) {
+    return vals.map(x => { return x * this.a + this.b })
+  }
+
+  fitTransform (vals) {
+    this.fit(vals)
+    return this.transform(vals)
+  }
+}
+
+class ColorScaler extends ValueScaler {
+  col1;
+  col2;
+  constructor (low, high) {
+    super(0.0, 1.0)
+    this.col1 = low
+    this.col2 = high
+  }
+
+  // super.fit(vals)
+
+  _process (s0, s1, r) {
+    const x0 = parseInt(s0)
+    const x1 = parseInt(s1)
+    if (isNaN(x0) || isNaN(x1)) return s0
+    return Math.floor(x0 + (x1 - x0) * r)
+  }
+
+  transform (vals) {
+    const s1 = this.col1.split(/(\d+)/)
+    const s2 = this.col2.split(/(\d+)/)
+    const _vals01 = vals.map(x => { return x * this.a + this.b })
+
+    return _vals01.map(v => s1.map((s, i) => this._process(s, s2[i], v)).join(''))
+  }
+}
+
+const maxEdgeWidth = 8
+
 export const preprocess = elements => {
-  const vals = elements.edges.map(ele => ele.data.weight)
-  const valmax = Math.max(...vals)
-  const valmin = Math.min(...vals)
-  const wmax = 6.0
-  const wmin = 1.0
-  const a = (wmax - wmin) / (valmax - valmin)
-  const b = wmin - valmin * (wmax - wmin) / (valmax - valmin)
-  elements.edges.forEach((ele, i) => { ele.data.weight = vals[i] * a + b })
+  //  edges
+  let arr = elements.edges.map(ele => ele.data.weight)
+
+  //  edge width
+  let res = (new ValueScaler(1.0, maxEdgeWidth)).fitTransform(arr)
+  elements.edges.forEach((ele, i) => { ele.data.weight = res[i] })
+
+  // console.log(elements.nodes)
+  // nodes
+  arr = elements.nodes.map(ele => ele.data.pagerank)
+
+  // node size
+  res = (new ColorScaler('6px', '24px')).fitTransform(arr)
+  elements.nodes.forEach((ele, i) => { ele.data.size = res[i] })
+
+  //  label size
+  res = (new ValueScaler(8, 12)).fitTransform(arr)
+  elements.nodes.forEach((ele, i) => { ele.data.font_size = res[i] })
 }
 
 export const preprocessPointCloud = elements => {
@@ -90,8 +158,6 @@ export const preprocessPointCloud = elements => {
   elements.nodes = nodes
   elements.edges = []
 }
-
-const maxEdgeWidth = 8
 
 const constrainWidth = width => {
   return (width ? (width < maxEdgeWidth ? width : maxEdgeWidth) : 1)

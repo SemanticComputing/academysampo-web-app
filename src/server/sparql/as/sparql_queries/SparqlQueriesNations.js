ee3e77d16faaeffda8482fd675546243d83e6f49
Petri Leskinen <petri.leskinen@aalto.fi>
2021-01-21 13:37:29 +0200

    Added query 'nationsPropertiesFacetResults'

--- src/server/sparql/as/sparql_queries/SparqlQueriesNations.js
+++ src/server/sparql/as/sparql_queries/SparqlQueriesNations.js
@@ -52,6 +52,34 @@ UNION
 }
 `
 
+export const nationsPropertiesFacetResults = `
+?id skos:prefLabel ?prefLabel__id .
+FILTER (LANG(?prefLabel__id)="fi")
+BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+
+BIND(?id as ?uri__id)
+BIND(?id as ?uri__dataProviderUrl)
+BIND(?id as ?uri__prefLabel)
+
+{
+    ?id skos:altLabel ?altLabel
+}
+UNION
+  { SELECT ?id (COUNT(DISTINCT ?prs) AS ?number_of_students) {
+    { ?prs :student_nation ?id }
+  UNION
+  { ?prs :has_event/:student_nation ?id }
+    ?prs a :Person
+  } GROUPBY ?id
+  }
+UNION
+{ 
+  ?id :wikidata ?externalLink__id. 
+  BIND ("Wikidata" AS ?externalLink__prefLabel)
+  BIND (?externalLink__id AS ?externalLink__dataProviderUrl)
+}
+`
+
 /** TODO this piece of query returns a different result than https://api.triplydb.com/s/c3WtDl3KB
  UNION
 {

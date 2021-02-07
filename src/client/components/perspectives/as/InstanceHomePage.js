73cf885b888b53e9aa92d3a68735b879083b0a09
esikkala <esko.ikkala@aalto.fi>
2021-02-07 15:25:27 +0200

    Improve instance page loaders

--- src/client/components/perspectives/as/InstanceHomePage.js
+++ src/client/components/perspectives/as/InstanceHomePage.js
@@ -128,6 +128,7 @@ class InstanceHomePage extends React.Component {
 
   render = () => {
     const { classes, tableData, isLoading, resultClass, rootUrl } = this.props
+    const hasTableData = this.hasTableData()
     return (
       <div className={classes.root}>
         <PerspectiveTabs
@@ -136,18 +137,18 @@ class InstanceHomePage extends React.Component {
           screenSize={this.props.screenSize}
         />
         <Paper square className={classes.content}>
-          {isLoading &&
+          {isLoading && !hasTableData &&
             <div className={classes.spinnerContainer}>
               <CircularProgress style={{ color: purple[500] }} thickness={5} />
             </div>}
-          {!this.hasTableData() &&
-            <>
+          {!hasTableData &&
+            <div className={classes.spinnerContainer}>
               <Typography variant='h6'>
                 No data found for id: <span style={{ fontStyle: 'italic' }}>{this.state.localID}</span>
               </Typography>
-            </>}
+            </div>}
           {/* make sure that tableData exists before rendering any components */}
-          {this.hasTableData() &&
+          {hasTableData &&
             <>
               <Route
                 exact path={`${rootUrl}/${resultClass}/page/${this.state.localID}`}
@@ -170,6 +171,7 @@ class InstanceHomePage extends React.Component {
                     results={this.props.results}
                     resultUpdateID={this.props.resultUpdateID}
                     fetchResults={this.props.fetchResults}
+                    fetching={isLoading}
                     resultClass='familyNetwork'
                     uri={tableData.id}
                     limit={150}
@@ -188,6 +190,7 @@ class InstanceHomePage extends React.Component {
                     results={this.props.results}
                     resultUpdateID={this.props.resultUpdateID}
                     fetchResults={this.props.fetchResults}
+                    fetching={isLoading}
                     resultClass='academicNetwork'
                     uri={tableData.id}
                     limit={200}
@@ -205,6 +208,7 @@ class InstanceHomePage extends React.Component {
                     results={this.props.results}
                     resultUpdateID={this.props.resultUpdateID}
                     fetchResults={this.props.fetchResults}
+                    fetching={isLoading}
                     resultClass='relationNetwork'
                     uri={tableData.id}
                     limit={200}
@@ -222,6 +226,7 @@ class InstanceHomePage extends React.Component {
                     results={this.props.results}
                     resultUpdateID={this.props.resultUpdateID}
                     fetchResults={this.props.fetchResults}
+                    fetching={isLoading}
                     resultClass='connections'
                     uri={tableData.id}
                     limit={60}

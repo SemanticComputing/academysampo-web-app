7009a52917d0ddfd1c1d9f825173d288a732ea3f
Petri Leskinen <petri.leskinen@aalto.fi>
2026-06-14 11:43:28 +0300

    Change prefixes

--- src/server/sparql/as/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/as/sparql_queries/SparqlQueriesPrefixes.js
@@ -4,13 +4,13 @@ PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
 PREFIX geosparql: <http://www.opengis.net/ont/geosparql#> 
 PREFIX gvp: <http://vocab.getty.edu/ontology#>
-PREFIX label: <http://ldf.fi/yoma/label/> 
+PREFIX label: <http://ldf.fi/yoma/label/>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX places: <http://ldf.fi/yoma/places/> 
+PREFIX places: <http://ldf.fi/yoma/places/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
 PREFIX rels: <http://ldf.fi/yoma/relations/> 
-PREFIX schema: <http://schema.org/> 
+PREFIX schema: <http://schema.org/>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#>
 PREFIX spatial: <http://jena.apache.org/spatial#>
